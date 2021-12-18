db4e0552d6ba41020c77ab051d090cfd5fbefdca
frontend <fontend@lexos.com.br>
2021-12-17 23:54:37 -0300

    filter sort

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -25,7 +25,7 @@ export function Card({
 }: CardProps){
     return (
         <>
-            <ContainerCard>
+            <ContainerCard >
                 <ImageCard>
                     <img src={image} alt="Pokemon Image" />
                 </ImageCard>
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { FormEvent, useEffect, useState } from 'react';
 import searchSvg from '../../assets/icons/search.svg';
 import { Card } from '../../components/Card';
 import { api } from '../../services/api';
@@ -19,32 +19,52 @@ interface PokemonDataModel {
 
 export function Home() {
     const [pokemonsData, setPokemonsData] = useState<PokemonDataModel[]>([]);
+    const [search, setSearch] = useState('');
+    const [select, setSelect] = useState('');
     
     useEffect(() => {
         api.get('pokedex.json')
         .then(response => setPokemonsData(response.data.pokemon))
     },  []);
 
-    console.log(pokemonsData);
+    const searchPokemon = pokemonsData.filter((pokemon) => pokemon.name.toLowerCase().startsWith(search.toLowerCase()) || pokemon.num.startsWith(search))
+
+    function sortFilter(event: string){
+        if(event === 'a'){
+            console.log('a')
+        } else {
+            console.log('b')
+        }
+    }
 
     return (
         <>
             <Container>
                 <Filters>
-                    <input type="text" name="search" id="search" placeholder="Digite nome ou número"/>
+                    <input 
+                        type="text" 
+                        name="search" 
+                        id="search" 
+                        placeholder="Digite nome ou número"
+                        value={search}
+                        onChange={event => setSearch(event.target.value)}
+                    />
                     <button>
                         <img src={searchSvg} alt="Ícone de pesquisar" />
                     </button>
                 </Filters>
                 <SelectFilter>
-                    <select name="sort" id="sort">
+                    <select name="sort" id="sort" value={select} onChange={event => sortFilter(event.target.value)}>
                         <option value="">Ordenar por...</option>
+                        <option value="a">A-Z</option>
+                        <option value="z">Z-A</option>
                     </select>
                 </SelectFilter>
                 <ContainerCards>
-                    {pokemonsData.map(data => {
+                    {searchPokemon.map(data => {
                         return (
                             <Card 
+                                key={data.id}
                                 id={data.id}
                                 image={data.img}
                                 numberPokemon={data.num}
